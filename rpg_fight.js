19026de902a2ae31ef0ec45e1a1ebda558f178b6
Christopher Tero <christopher.tero@gmail.com>
2019-11-12 13:17:08 -0600

    reorganized and cleaned up code

--- rpg_fight.js
+++ rpg_fight.js
@@ -2,25 +2,39 @@ const monsterAttackValue = document.querySelector("#monsterAttackValue");
 const monsterDefenseValue = document.querySelector("#monsterDefenseValue");
 const monsterHpValue = document.querySelector("#monsterHpValue");
 const monsterTypeValue = document.querySelector("#monsterTypeValue");
-let attack = 10;
-let defense = 5;
-let hp = 40;
+let monsterAttack = 10;
+let monsterDefense = 5;
+let monsterHp = 40;
+let monsterType = "Goblin";
 
 
 const hero = document.querySelector("#hero");
 
 function monsterAttackValueFunction() {
-  monsterAttackValue.textContent = attack;
+  monsterAttackValue.textContent = monsterAttack;
 }
 
 function monsterDefenseValueFunction() {
-  monsterDefenseValue.textContent = defense;
+  monsterDefenseValue.textContent = monsterDefense;
 }
 
 function monsterHpValueFunction() {
-  monsterHpValue.textContent = hp;
+  monsterHpValue.textContent = monsterHp;
 }
 
-monsterAttackValueFunction();
-monsterDefenseValueFunction();
-monsterHpValueFunction();
+function monsterTypeValueFunction() {
+  monsterTypeValue.textContent = monsterType;
+}
+
+function createMonster() {
+  monsterAttackValueFunction();
+  monsterDefenseValueFunction();
+  monsterHpValueFunction();
+  monsterTypeValueFunction();
+}
+
+function runGame() {
+  createMonster();
+}
+
+runGame();
