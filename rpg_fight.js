c118d83e3a923d1f3185070c4473744b9b0402ac
Christopher Tero <christopher.tero@gmail.com>
2019-11-12 14:34:46 -0600

    added all hero stat functions

--- rpg_fight.js
+++ rpg_fight.js
@@ -1,29 +1,58 @@
+const heroAttackValue = document.querySelector("#heroAttackValue");
+const heroDefenseValue = document.querySelector("#heroDefenseValue");
+const heroHpValue = document.querySelector("#heroHpValue");
+const heroClassValue = document.querySelector("#heroClassValue");
 const monsterAttackValue = document.querySelector("#monsterAttackValue");
 const monsterDefenseValue = document.querySelector("#monsterDefenseValue");
 const monsterHpValue = document.querySelector("#monsterHpValue");
 const monsterTypeValue = document.querySelector("#monsterTypeValue");
-let monsterAttack = 10;
-let monsterDefense = 5;
-let monsterHp = 40;
-let monsterType = "Goblin";
 
 
-const hero = document.querySelector("#hero");
+function heroAttackValueFunction() {
+  let heroAttack = 10;
+  heroAttackValue.textContent = heroAttack;
+}
+
+function heroDefenseValueFunction() {
+  let heroDefense = 10;
+  heroDefenseValue.textContent = heroDefense;
+}
+
+function heroHpValueFunction() {
+  let heroHp = 50;
+  heroHpValue.textContent = heroHp;
+}
+
+function heroClassValueFunction() {
+  let heroClass = "Ranger";
+  heroClassValue.textContent = heroClass;
+}
 
 function monsterAttackValueFunction() {
+  let monsterAttack = 10;
   monsterAttackValue.textContent = monsterAttack;
 }
 
 function monsterDefenseValueFunction() {
+  let monsterDefense = 5;
   monsterDefenseValue.textContent = monsterDefense;
 }
 
 function monsterHpValueFunction() {
+  let monsterHp = 40;
   monsterHpValue.textContent = monsterHp;
 }
 
 function monsterTypeValueFunction() {
+  let monsterType = "Goblin";
   monsterTypeValue.textContent = monsterType;
+}
+
+function createHero() {
+  heroAttackValueFunction();
+  heroDefenseValueFunction();
+  heroHpValueFunction();
+  heroClassValueFunction();
 }
 
 function createMonster() {
@@ -34,6 +63,7 @@ function createMonster() {
 }
 
 function runGame() {
+  createHero();
   createMonster();
 }
 
